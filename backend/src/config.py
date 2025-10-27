37440a79fd9fa0c79d38e433e0a8b05ffffa6695
Denis <baraden3.0@gmail.com>
2025-10-27 12:58:40 +0700

    some routers + their logic

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,3 +1,4 @@
 # тут будут настройки env, S3, db, redis
 REDIS_URL = 'redis://redis:6379/0'
-POSTGRES_URL = 'postgresql://user:pwd1234@db/coonspect'
+DATABASE_URL = 'postgresql://user:pwd1234@db/coonspect'
+STT_SERVICE_URL = "http://stt-service:8000/transcribe"
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from src.api.routers.users import router as user_router
+from src.api.routers.lectures import router as lecture_router
 
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     return {"status": "ok"}
+
+app.include_router(user_router)
+app.include_router(lecture_router)
